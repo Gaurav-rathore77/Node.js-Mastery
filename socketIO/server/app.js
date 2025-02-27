d92bd845eeea4bd5f9d3f0a68adf6d5389b9a2f3
Gaurav-rathore77 <vimleshrathor1984@gmail.com>
2025-02-28 01:57:36 +0530

    add socket IO

---
+++ socketIO/server/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        // credentials: true,
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+server.listen(8000, () => {
+    console.log(`Server is running on http://localhost:8000`);
+});
